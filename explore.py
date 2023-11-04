6e5ce5aa9d709f3812ca94ede4eff3261e69b1f3
suffering <root@DESKTOP-6BN0U07.localdomain>
2023-11-04 16:46:58 -0700

    feat: implement e-greedy.

--- explore.py
+++ explore.py
@@ -1,6 +1,12 @@
+import numpy as np
+
 from mdp import MaximumLikelihoodMDP
 
-def e_greedy(model: MaximumLikelihoodMDP, epsilon: float, s: int) -> int:
+# sure, why  not. have some globals.
+alpha = 0.99
+epsilon = 0.2
+
+def e_greedy(model: MaximumLikelihoodMDP, s: int) -> int:
     """Returns next action to take.
 
     Args:
@@ -11,4 +17,16 @@ def e_greedy(model: MaximumLikelihoodMDP, epsilon: float, s: int) -> int:
     Returns:
       action (in model representation) to take.
     """
-    raise NotImplementedError("e_greedy() not implemented.")
+    prev_epsilon = epsilon
+    epsilon = epsilon * alpha
+    if np.random.random() < prev_epsilon:
+        return np.random.choice(model.actions())
+
+    max_a = 1
+    q_max = -np.inf
+    for action in model.actions():
+        q_sa = model.lookahead(s, action)
+        if q_sa > q_max:
+            q_max = q_sa
+            max_a = action
+    return max_a
